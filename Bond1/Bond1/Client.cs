433fd99ffafce1de844ee39b7d6ec71e80575f3d
susumOyaji <sumitomoshigeru@sumitomoshigeru-no-MacBook-Pro.local>
2017-08-01 22:42:04 +0900

    mac to push

--- Bond1/Bond1/Client.cs
+++ Bond1/Bond1/Client.cs
@@ -14,6 +14,7 @@ namespace Bond1
 
         public Client()
         {
+            BackgroundColor = Color.BlueViolet;
             ClientReturn();
         }
 
@@ -21,11 +22,11 @@ namespace Bond1
         public string ClientReturn()
         {
             // Imageビューの生成
-            var image = new Image { Aspect = Aspect.Fill };
-            image.Source = ImageSource.FromFile("Menu15.png");
+            var image = new Image { Aspect = Aspect.AspectFit};
+            image.Source = ImageSource.FromFile("Menu2.png");
 
             ////さらに良いことに、暗黙の変換があるので、この行も機能します：
-            image.Source = "Menu15.png";
+            image.Source = "Menu2.png";
 
 
 
@@ -112,108 +113,144 @@ namespace Bond1
             };
 
 
-
-           
-
-
-
-            /// <summary>
-            /// </summary>
-            /// <param name="sender">Sender.</param>
-            /// <param name="e">E.</param>
-
-            var fastgrid = new Grid
-            {
-                HorizontalOptions = LayoutOptions.FillAndExpand,
-                VerticalOptions = LayoutOptions.Center,
-                RowDefinitions =
-            {
-                    new RowDefinition() { Height = GridLength.Auto}//行
-            },
-                ColumnDefinitions =//桁
-            {
-                new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) },
-                new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) },
-                //new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) },
-            }
-            };
-
-            fastgrid.Children.Add(new Label
-            {
-                VerticalOptions = LayoutOptions.Center,
-                //HorizontalOptions = LayoutOptions.FillAndExpand,
-                //HeightRequest = 2,
-                Text = "This My IpAdress",
-                TextColor = Color.Black,
-                BackgroundColor = Color.Aqua,
-            }, 0, 0);
-
-            fastgrid.Children.Add(new Button
-            {
-                //VerticalOptions = LayoutOptions.Center,
-                //HorizontalOptions = LayoutOptions.FillAndExpand,
-                Text = ipAdr.getIPAddress(),//"two two",
-                TextColor = Color.Black,
-                BackgroundColor = Color.Fuchsia,
-            }, 1, 0);
-
-            //fastgrid.Children.Add(new Button
-            //{
-            //    VerticalOptions = LayoutOptions.Center,
-            //    HorizontalOptions = LayoutOptions.FillAndExpand,
-            //    Text = "three three three",
-            //    TextColor = Color.Black,
-            //    BackgroundColor = Color.Lime,
-            //}, 2, 0);
-
-
-            var secondgrid = new Grid
-            {
-                HorizontalOptions = LayoutOptions.FillAndExpand,
-                VerticalOptions = LayoutOptions.Center,
-                RowDefinitions =
-            {
-                new RowDefinition() { Height = GridLength.Auto }
-            },
-                ColumnDefinitions =
-            {
-                new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) },
-                new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) },
-                //new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) },
-            }
-            };
             ///
-           
-            secondgrid.Children.Add(IpaEntry = new Entry
+            AbsoluteLayout absoluteLayout = new AbsoluteLayout
             {
-                //VerticalOptions = LayoutOptions.Center,
-                //HorizontalOptions = LayoutOptions.FillAndExpand,
-                HeightRequest = 35,
-                Text = "Input to IpAdress",
-                TextColor = Color.Black,
-                BackgroundColor = Color.Aqua,
-            }, 0, 0);
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
 
-            secondgrid.Children.Add(SendIpa = new Button
+            // Labelを生成する
+            Label label = new Label
             {
-                //VerticalOptions = LayoutOptions.Center,
-                //HorizontalOptions = LayoutOptions.FillAndExpand,
-                Text = "接続",//"two two",
-                TextColor = Color.Black,
-                BackgroundColor = Color.Fuchsia,
-            }, 1, 0);
+                Text = "中心(0.5,0.5)",
+                FontSize = 22,
+                TextColor = Color.Yellow,
+            };
 
-            //secondgrid.Children.Add(new Button
-            //{
-            //    VerticalOptions = LayoutOptions.Center,
-            //    HorizontalOptions = LayoutOptions.FillAndExpand,
-            //    Text = "three three three",
-            //    TextColor = Color.Black,
-            //    BackgroundColor = Color.Lime,
-            //}, 2, 0);
+            // Labelを生成する
+            Label label1 = new Label
+            {
+                Text = "左上(0,0)",
+                FontSize = 24,
+                TextColor = Color.Yellow,
+            };
 
-            SendIpa.Clicked += SendIpaButton_Clicked;
+            // Labelを生成する
+            Label label2 = new Label
+            {
+                Text = "右上(1,0)",
+                FontSize = 24,
+                TextColor = Color.Yellow,
+            };
 
+            // Labelを生成する
+            Label label3 = new Label
+            {
+                Text = "左下(0,1)",
+                FontSize = 24,
+                TextColor = Color.Yellow,
+            };
+
+            // Labelを生成する
+            Label label4 = new Label
+            {
+                Text = "右下(1,1)",
+                FontSize = 24,
+                TextColor = Color.Yellow,
+            };
+
+            // Labelを生成する
+            Label label01 = new Label
+            {
+                Text = "上(0.5,0)",
+                FontSize = 24,
+                TextColor = Color.Yellow,
+            };
+
+            // Labelを生成する
+            Label label02 = new Label
+            {
+                Text = "左中心(0,0.5)",
+                FontSize = 22,
+                TextColor = Color.Yellow,
+            };
+
+            // Labelを生成する
+            Label label03 = new Label
+            {
+                Text = "右中心(1,0.5)",
+                FontSize = 22,
+                TextColor = Color.Yellow,
+            };
+
+            // Labelを生成する
+            Label label04 = new Label
+            {
+                Text = "下(0.5,1)",
+                FontSize = 24,
+                TextColor = Color.Yellow,
+            };
+
+            // ラベルを配置
+            absoluteLayout.Children.Add(image/* label*/); // 中心
+            absoluteLayout.Children.Add(label1); // 左上
+            absoluteLayout.Children.Add(label2); // 右上
+            absoluteLayout.Children.Add(label3); // 左下
+            absoluteLayout.Children.Add(label4); // 右下
+
+            absoluteLayout.Children.Add(label01); // 上
+            //absoluteLayout.Children.Add(ServerButton/*label02*/); // 左中心
+            //absoluteLayout.Children.Add(ClientButton/*label03*/); // 右中心
+            absoluteLayout.Children.Add(label04); // 下
+
+            // 左上
+            AbsoluteLayout.SetLayoutFlags(label1, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(label1, new Rectangle(0, 0, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+
+            // 上
+            AbsoluteLayout.SetLayoutFlags(label01, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(label01, new Rectangle(0.5, 0, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+
+            // 右上
+            AbsoluteLayout.SetLayoutFlags(label2, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(label2, new Rectangle(1, 0, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+
+            // 左中心
+            //AbsoluteLayout.SetLayoutFlags(ServerButton/*label02*/, AbsoluteLayoutFlags.PositionProportional);
+            //AbsoluteLayout.SetLayoutBounds(ServerButton/* label02*/, new Rectangle(0, 0.5, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+
+            // 中心
+            AbsoluteLayout.SetLayoutFlags(image/* label*/, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(image/* label*/, new Rectangle(0.5, 0.0, 100/*AbsoluteLayout.AutoSize*/, 100/*AbsoluteLayout.AutoSize*/));
+
+            // 右中心
+            //AbsoluteLayout.SetLayoutFlags(ClientButton/*label03*/, AbsoluteLayoutFlags.PositionProportional);
+            //AbsoluteLayout.SetLayoutBounds(ClientButton/*label03*/, new Rectangle(1.0, 0.5, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+
+            // 左下
+            AbsoluteLayout.SetLayoutFlags(label3, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(label3, new Rectangle(0, 1, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+
+            // 下
+            AbsoluteLayout.SetLayoutFlags(label04, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(label04, new Rectangle(0.5, 1, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+
+            // 右下
+            AbsoluteLayout.SetLayoutFlags(label4, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(label4, new Rectangle(1, 1, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+
+            // absoluteLayoutを配置
+            //Content = absoluteLayout;
+            //Content = SelectDisp;
+
+
+
+
+
+
+
+
+       
 
 
 
@@ -249,8 +286,9 @@ namespace Bond1
             
 
 
-            Content = ClientDisp;
-            //Content = grid;
+            //Content = ClientDisp;
+            Content = absoluteLayout;
+            //Content = secondgrid;
 
 
 
